c17ac28ee3c13085eb69c84e981fa57b2e782ee0
Brwonggg <Brandonwlc360@gmail.com>
2026-07-19 10:48:41 +0800

    Added EncoderBlock and Encoder class

--- model.py
+++ model.py
@@ -114,3 +114,31 @@ class ResConnection(nn.Module):
 
     def forward(self, x, sublayer):
         return x + self.dropout(sublayer(self.norm(x)))
+    
+class EncoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.res_connections = nn.ModuleList([ResConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.res_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.res_connections[1](x, self.feed_forward_block)
+        return x
+    
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+
+
+
