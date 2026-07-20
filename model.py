d40311c3365983e09913488e3397a72888bd1caa
Brwonggg <Brandonwlc360@gmail.com>
2026-07-20 08:40:41 +0800

    Added Transformer class which consists of encoder, decoder and projection

--- model.py
+++ model.py
@@ -171,7 +171,30 @@ class ProjectionLayer(nn.Module):
     def forward(self, x):
         # (Batch, Seq_Len, d_model) -> (Batch, Seq_Len, Vocab_Size)
         return torch.log_softmax(self.proj(x), dim = -1)
+    
+class Transformer(nn.Module):
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbedding, tgt_embed: InputEmbedding, src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer):     
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
 
+    def encode(self, src, src_mask):
+        src = self.src_embed(src)
+        src = self.scr_pos(src)
+        return self.encoder(src, src_mask)
+    
+    def decode(self, encoder_output, src_mask, tgt, tgt_mask):
+        tgt = self.tgt_embed(tgt)
+        tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+    
+    def project(self, x):
+        return self.projection_layer(x)
 
 
 
